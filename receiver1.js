1ad73fef10eb0ebb8fc56c991f2aab77c2ab04e3
DmitrySmetyukhov <dmitry.smetyukhov@gmail.com>
2017-07-28 14:45:29 +0300

    routing

--- receiver1.js
+++ receiver1.js
@@ -1,18 +1,31 @@
 var amqp = require('amqplib/callback_api');
 
 
+var args = process.argv.slice(2);
+
+if (args.length == 0) {
+    console.log("Usage: receive_logs_direct.js [info] [warning] [error]");
+    process.exit(1);
+}
+
+// node receiver1.js info
+// node receiver.js warning ...
+
 amqp.connect('amqp://sgnaandf:N4Pzd2U4utaOgGMklwkf8g3Kl4L-tFQn@wasp.rmq.cloudamqp.com/sgnaandf', function (err, conn) {
     conn.createChannel(function(err, ch) {
-        var ex = 'logs';
+        var ex = 'direct_logs';
 
-        ch.assertExchange(ex, 'fanout', {durable: false});
+        ch.assertExchange(ex, 'direct', {durable: false});
 
         ch.assertQueue('', {exclusive: true}, function(err, q) {
-            console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", q.queue);
-            ch.bindQueue(q.queue, ex, '');
+            console.log(' [*] Waiting for logs. To exit press CTRL+C');
+
+            args.forEach(function(severity) {
+                ch.bindQueue(q.queue, ex, severity);
+            });
 
             ch.consume(q.queue, function(msg) {
-                console.log(" [x] %s", msg.content.toString());
+                console.log(" [x] %s: '%s'", msg.fields.routingKey, msg.content.toString());
             }, {noAck: true});
         });
     });
--- sender.js
+++ sender.js
@@ -6,16 +6,21 @@ rl.prompt();
 
 amqp.connect('amqp://sgnaandf:N4Pzd2U4utaOgGMklwkf8g3Kl4L-tFQn@wasp.rmq.cloudamqp.com/sgnaandf').then(function (conn) {
     return conn.createChannel().then(function (ch) {
-        var ex = 'logs';
-        var ok = ch.assertExchange(ex, 'fanout', {durable: false});
+        var ex = 'direct_logs';
+        var ok = ch.assertExchange(ex, 'direct', {durable: false});
         // var ok = ch.assertQueue(q, {durable: true});
 
         return ok.then(function (_qok) {
             rl
                 .on('line', function (line) {
+                    let severity = line.split(',')[0];
+
+                    // info, hello
+                    // warning, hello
+
                     if (line === "exit") rl.close();
                     rl.prompt();
-                    ch.publish(ex, '', new Buffer(line));
+                    ch.publish(ex, severity, new Buffer(line));
                     // ch.sendToQueue(q, Buffer.from(line), {persistent: true});
                     console.log(" [x] Sent '%s'", line);
 
